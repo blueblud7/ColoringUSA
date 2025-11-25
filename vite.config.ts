c52a5971917e94b48cb4725cb0036b84a4478ca9
Sangwon Chae <sangwonchae@Sangwonui-MacBookPro.local>
2025-11-25 10:54:32 -0800

    GitHub Pages base 경로를 ColoringUSA로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/Coloring/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/ColoringUSA/' : '/',
   build: {
     outDir: 'dist',
   },
